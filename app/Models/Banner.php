e693875ae45a5eb1b69730f67f63dc68a4c18eee
Foxbat323 <coknindya67@gmail.com>
2026-05-10 02:32:14 +0800

    test

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -4,7 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+<<<<<<<< HEAD:app/Models/Banner.php
 class Banner extends Model
+========
+class ReportPost extends Model
+>>>>>>>> 8c6cfbb (test):app/Models/ReportPost.php
 {
     //
 }
