7ee5f0036c4129281559ed4dfa4522a54cb765b3
Foxbat323 <coknindya67@gmail.com>
2026-05-10 09:42:07 +0800

    making resources of reportPost and reportTransactions

--- app/Models/ReportPost.php
+++ app/Models/ReportPost.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
 class ReportPost extends Model
 {
-    //
+    use HasUuids;
+
+    protected $guarded = [];
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+   
+    public function reporter()
+    {
+        return $this->belongsTo(User::class, 'reporter_id');
+    }
 }
