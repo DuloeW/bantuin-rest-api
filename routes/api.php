8c7676ac32e3bc0e0c2ba910afee5930bdd6a116
DuloeW <duloekeren@gmail.com>
2026-05-24 12:11:49 +0800

    Change order in API routes

--- routes/api.php
+++ routes/api.php
@@ -15,24 +15,24 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/users/profile', [UserController::class, 'getProfile']);
     
     Route::get('/users', [UserController::class, 'getAll']);
-    Route::get('/users/{id}', [UserController::class, 'getById']);
+    Route::put('/users', [UserController::class, 'update']);
     Route::get('/users/first-name/{name}', [UserController::class, 'getByFirstName']);
     Route::get('/users/last-name/{name}', [UserController::class, 'getByLastName']);
-    Route::put('/users', [UserController::class, 'update']);
+    Route::get('/users/{id}', [UserController::class, 'getById']);
 
     Route::get('/categories', [CategoryController::class, 'getAll']);
     Route::post('/categories', [CategoryController::class, 'create']);
+    Route::get('/categories/slug/{slug}', [CategoryController::class, 'getBySlug']);
     Route::get('/categories/{id}', [CategoryController::class, 'getById']);
     Route::delete('/categories/{id}', [CategoryController::class, 'delete']);
-    Route::get('/categories/slug/{slug}', [CategoryController::class, 'getBySlug']);
 
     Route::get('/posts', [PostController::class, 'getAll']);
-    Route::delete('/posts/{id}', [PostController::class, 'delete']);
     Route::get('/posts/total', [PostController::class, 'getTotalUserPosts']);
     Route::post('/posts/request', [PostController::class, 'createRequest']);
     Route::post('/posts/service', [PostController::class, 'createService']);
     Route::get('/posts/request', [PostController::class, 'getAllWithRequestDetails']);
     Route::get('/posts/service', [PostController::class, 'getAllWithServiceDetails']);
+    Route::delete('/posts/{id}', [PostController::class, 'delete']);
     
     Route::post('/posts/apply', [OfferController::class, 'applyForJob']);
     Route::post('/posts/book-helper', [OfferController::class, 'bookHelperService']);
