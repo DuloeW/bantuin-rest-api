3fa1c16a3480beb8ac78cf45d578577b323b7ee5
DuloeW <duloekeren@gmail.com>
2026-06-28 23:26:46 +0800

    add skil response

--- app/Models/Skill.php
+++ app/Models/Skill.php
@@ -3,10 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Attributes\Guarded;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
 #[Guarded([])]
+#[Hidden(['pivot'])]
 class Skill extends Model
 {
 
